48eee8f91c7506cf0dbaa2e141b0991dbf023c94
EC2 Default User <ec2-user@ip-172-31-45-199.ec2.internal>
2021-10-14 11:06:29 +0000

    [add]コントローラの定義を変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,38 +1,6 @@
 class HomesController < ApplicationController
-   def index
-    @books = Book.all
-    @books = Book.new
-   end
 
-  def show
-    @books = Book.find(params[:id])
-  end
-
-  def create
-   @book = Book.new(book_params[:id])
-   book.save
-   redirect_to book_path
-  end
-
-  def edit
-    @book = Book.find(params[:id])
-  end
-
-  def update
-    @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
-  end
-
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
-  end
-
-private
-  def book_params
-    params.permit(:title, :body)
+  def top
   end
 
 end
